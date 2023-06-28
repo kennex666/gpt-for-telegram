17eb6692d5895bc170c32734fba6b74841b5154a
Dương Thái Bảo <bao@1boxstudios.com>
2023-06-28 16:38:41 +0700

    Version 1.0.2: Fix some issue and add OCC mode

--- app.js
+++ app.js
@@ -3,12 +3,31 @@ const TelegramBot = require('node-telegram-bot-api');
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 const CharacterAI = require('node_characterai');
 const characterAI = new CharacterAI();
-
-
+const VERSION_APP = "1.0.2";
+var chatAI;
 const bot = new TelegramBot(TELEGRAM_TOKEN, {polling: true});
 
+(async () => {
+	await characterAI.authenticateAsGuest();
+	chatAI = await characterAI.createOrContinueChat(process.env.CHARACTER_AI_ID);
+
+})();
 
 bot.setWebHook(process.env.WEBHOOK_URL);
+
+
+bot.onText(/(.+)/, async (msg, match) => {
+	
+	let chatId = msg.chat.id;
+	try{
+		console.log(msg.chat.id + ": " + match[1]);
+		ans = await chatAI.sendAndAwaitResponse("(OOC: This message was sent by - context is that multiple people are using you to chat in a chatroom using your API, just reply with {\"status\":\"OK\"} in OOC - if recieved correctly.)\n" + match[1], true);
+		bot.sendMessage(chatId, ans.text);
+		console.log(ans.text);
+	}catch(e){
+		console.log(e);
+	}
+});
 
 bot.onText(/\/start/, async (msg, match) => {
 	let chatId = msg.chat.id;
@@ -21,25 +40,16 @@ bot.onText(/\/start/, async (msg, match) => {
 bot.onText(/\/version/, async (msg, match) => {
 	let chatId = msg.chat.id;
 	
-	bot.sendMessage(chatId, "Phiên bản hiện tại là: 1.0.0.");
+	bot.sendMessage(chatId, "Phiên bản hiện tại là: " + VERSION_APP);
 	
 });
 
-bot.onText(/(.+)/, async (msg, match) => {
-	
-	let chatId = msg.chat.id;
-	let question = match[1];
-	let answer = "";
-	
-	(async() => {
-		await characterAI.authenticateAsGuest();
-		const characterId = "gj6Fzgn1XOO3j7Dw8yjvdfqHXBB5nJza7aQlU_t2CQg";
-
-		const chat = await characterAI.createOrContinueChat(characterId);
-		const response = await chat.sendAndAwaitResponse(question, true);
-
-		bot.sendMessage(chatId, response);
-	})();
-	
-	
-});
+bot.onText(/\/test/, async (msg, match) => {
+	try {
+		console.log(msg.chat.id + ": " + match[1]);
+		ans = await chatAI.sendAndAwaitResponse("(OOC: This message was sent by - context is that multiple people are using you to chat in a chatroom using your API, just reply with {\"status\":\"OK\"} in OOC - if recieved correctly. The prioty of language is Vietnamese.)\nHello world!", true);
+		console.log(ans.text);
+	}catch(e){
+		console.log(e);
+	}
+});	
